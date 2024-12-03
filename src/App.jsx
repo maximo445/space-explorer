2023ad07fc94c3a8ce36d264dc9eddc4893bc818
Maximo Brito <britofilmmaker@gmail.com>
2024-12-03 09:53:18 -0500

    tailwind css added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import PicOfDay from "./components/PicOfDay";
 function App() {
   return (
     <div>
-      <PicOfDay></PicOfDay>
+      <h1>Comming Soon</h1>
+      <div className="h-screen w-full flex justify-center items-center">
+        <PicOfDay />
+      </div>
     </div>
   );
 }
