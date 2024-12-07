ca6c07207988ab4011ca3624b2ff8c3c24e8f9d2
Maximo Brito <britofilmmaker@gmail.com>
2024-12-07 15:05:07 -0500

    stablished days input conditionally on MarsInputTaker component - working on picking a rover for pictures

--- src/components/MarsInputTaker.jsx
+++ src/components/MarsInputTaker.jsx
@@ -1,18 +1,57 @@
+import { useState } from "react";
+
 function MarsInputTaker() {
+  const [searchType, setSearchType] = useState("randon");
+
   return (
     <div className="w-1/2">
-      <h1>Explore Mars</h1>
-      <div>
+      <div className="flex justify-center w-full p-8">
+        <h1 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-500 to-indigo-500">
+          Explore Mars
+        </h1>
+      </div>
+
+      <div className="p-10">
         <div className="flex justify-between items-center">
-          <select id="dropdown" name="options">
-            <option value="option1">Option 1</option>
-            <option value="option2">Option 2</option>
-            <option value="option3">Option 3</option>
+          <select
+            value={searchType}
+            onChange={(e) => setSearchType(e.target.value)}
+            id="dropdown"
+            name="options"
+          >
+            <option value="randon">Randon Day</option>
+            <option value="single">Single Day</option>
+            <option value="range">Range</option>
           </select>
-          <span>
-            <input type="date" name="start" id="start" />
-            <input type="date" name="end" id="end" />
-          </span>
+          {searchType !== "randon" && (
+            <span
+              className={
+                searchType === "single "
+                  ? "flex justify-start w-full"
+                  : "flex justify-around w-full"
+              }
+            >
+              <span className="flex flex-col">
+                <label htmlFor="start">Start Date</label>
+                <input type="date" name="start" id="start" />
+              </span>
+              {searchType !== "single" && (
+                <span className="flex flex-col">
+                  <label htmlFor="end">End Date</label>
+                  <input type="date" name="end" id="end" />
+                </span>
+              )}
+            </span>
+          )}
+          <div>
+            <label htmlFor="rover">Choose a Mars Rover:</label>
+            <select id="rover" name="rover">
+              <option value="FHAZ"> Front Hazard Avoidance Camera</option>
+              <option value="RHAZ">Rear Hazard Avoidance Camera</option>
+              <option value="MAST">Mast Camera</option>
+            </select>
+          </div>
+          <button type="submit">Submit</button>
         </div>
       </div>
     </div>
